6f3725eb706991e8bcc15378ff378cac2d880a3a
Renan Oliveira <isacrenan@hotmail.com>
2024-09-11 10:28:04 -0300

    02

---
+++ SQL/02 Análise de Vendas por Categoria de Produto.sql
@@ -0,0 +1,15 @@
+
+
+USE BikeStores
+
+-- Análise de Vendas por Categoria de Produto
+SELECT
+    C.CategoryID AS Categoria,
+    C.[Name] AS 'Nome da Categoria',
+    FORMAT(SUM(S.OrderTotal), 'C') AS Total
+FROM Production.[Category] C
+INNER JOIN Production.[Product] P ON P.CategoryID = C.CategoryID
+INNER JOIN Sales.[OrderItem] O ON O.ProductID = P.ProductID
+INNER JOIN Sales.[Order] S ON S.OrderID = O.OrderID
+GROUP BY C.CategoryID, C.[Name]
+ORDER BY Total DESC;
